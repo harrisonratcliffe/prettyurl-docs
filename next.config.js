e430db4ba6dbed60c3cb66b6b8f8347e822cf867
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2024-11-05 13:51:48 +0000

    @cloudflare/next-on-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+if (process.env.NODE_ENV === 'development') {
+  setupDevPlatform();
+}
+
+module.exports = withNextra();
